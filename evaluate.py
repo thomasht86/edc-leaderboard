3c4f6230fe3328dadc352af41a0778a5538b580b
thomasht86 <thomas.h.thoresen@gmail.com>
2022-09-05 12:26:49 +0200

    first

---
+++ evaluate.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import pandas as pd 
+
+#df_test = pd.read_csv("df_test.csv")
+
+def validate_df(df):
+    # Validate columns and index of df
+    return True
+
+def score_df(df):
+    # Calculate nrmse of dataframe
+    return np.random.random()
---
+++ main.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import databutton as db
+import streamlit as st
+import pandas as pd
+import streamlit as st
+import pandas as pd
+import numpy as np
+from evaluate import validate_df, score_df
+
+columns = ["Participant", "Submission name", "Timestamp", "Score"]
+participants = ["P1", "P2", "P3"]
+def get_df():
+    df = pd.DataFrame(columns=columns, data=[[p, " This is a description of the submission ", datetime.now(), 0] for p in participants])
+    return df
+
+df = get_df()
+db.storage.dataframes.put(df, "leaderboard")
+
+@db.apps.streamlit(route="/leaderboard", name="Leaderboard")
+def leaderboard():
+    st.set_page_config(
+     page_title="EDC Leaderboard",
+     page_icon="🔮")
+    st.title(" EDC AzureML Hackathon 2022 ")
+    df = db.storage.dataframes.get("leaderboard")
+    placeholder = st.empty()
+    with placeholder.container():
+        ## Sidebar
+        st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/c/c6/Equinor.svg", width=200)
+        submitter = st.sidebar.selectbox("Submitter", participants)
+        submission_name = st.sidebar.text_input("Submission name")
+        uploaded_file = st.sidebar.file_uploader("Upload a CSV file",type="csv")
+        button_placeholder = st.sidebar.empty()
+        button_placeholder.button("Submit", key="sb", disabled=True)
+        if uploaded_file is not None:
+                sub = pd.read_csv(uploaded_file)
+                valid = validate_df(sub)
+                if valid:
+                    submit = button_placeholder.button("Submit", key="sb2", disabled=False)
+                    if submit:
+                        score = score_df(sub)
+                        new_row = pd.DataFrame(columns=columns, data=[[submitter, submission_name, datetime.now(), score]])
+                        df = df.append(new_row, ignore_index=True)
+                        df = df.sort_values(by="Score", ascending=True)
+                        db.storage.dataframes.put(df, "leaderboard")
+                        st.sidebar.success("Submission successful")
+                        st.sidebar.balloons()
+                        submit = False
+                        uploaded_file = None
+        st.dataframe(df) 
+                     
+    st.sidebar.info(
+            f"""
+                👆 Upload a .csv file to make your submission. 
+                Must be formatted like this: [sample_submission.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
+                """
+        )
+    
+    
+
+@db.jobs.repeat_every(seconds=10 * 60)
+def repeating_job():
+    # Check for new data
+    # Do some work on that data
+    # Write that data to db.dataframes
+    # Send slack notification
+    print("Success!")
